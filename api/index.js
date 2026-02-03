705e5102959350399a6f76dcd46399a1ba748e82
Yovo Mihov <yovomihov05@gmail.com>
2026-02-03 19:56:39 +0200

    bcrypt doesn't work on Vercel serverless, fix

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const { neon } = require('@neondatabase/serverless')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const sql = neon(process.env.DATABASE_URL)
